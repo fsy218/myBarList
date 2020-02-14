f4aa849b3f6889a6da1fc7f33087a7a6215be164
fsy218 <999fsy218@gmail.com>
2020-02-14 23:24:42 +0900

    Update SearchViewController.swift

--- myBarList/SearchViewController.swift
+++ myBarList/SearchViewController.swift
@@ -7,104 +7,140 @@
 //
 
 import UIKit
+import SafariServices
+import RealmSwift
 
-class SearchViewController: UIViewController, UISearchBarDelegate {
-
+class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchText.delegate = self
         searchText.placeholder = "キーワードを入力してください"
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        guard let prefdata = try? getJSONData() else { return }
+        guard let prefList = try? JSONDecoder().decode([PrefCodeGnavi].self, from: prefdata) else { return }
+        print(prefList[0])
+
     }
 
+    func getJSONData() throws -> Data? {
+        guard let path = Bundle.main.path(forResource: "PrefCodeGnavi", ofType: "json") else { return nil }
+        let url = URL(fileURLWithPath: path)
+        return try Data(contentsOf: url)
+    }
+    
+    
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var restList : [(id:String, name:String, category:String, url_mobile:String, shop_image1:String, address:String, tel:String, line:String, station:String, walk:String, pr_short:String, areaname:String, prefname:String, areaname_s:String, category_name_l:[String])] = []
+    var restList: [RestGnavi] = []
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         view.endEditing(true)
         if let searchWord = searchBar.text {
             print(searchWord)
-            searchShop(keyword: searchWord)
+            searchRestaurant(keyword: searchWord)
         }
     }
     
-    struct RestJson: Codable {
-        let id: String?
-        let name: String?
-        let category: String?
-        let url_mobile: String?
-        let image_url: ImageUrl?
-        let address: String?
-        let tel: String?
-        let access: Access?
-        let pr: Pr?
-        let code: Code?
+    func searchRestaurant(keyword: String) {
+    
+       guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+           return
+       }
+       guard let req_url = URL(string: "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=6bf4be830455182560cb7ad8ba817c16&freeword=\(keyword_encode)&hit_per_page=10") else {
+           return
+       }
+       print(req_url)
         
-        struct ImageUrl: Codable {
-            let shop_image1: String?
-        }
-        struct Access: Codable {
-            let line: String?
-            let station: String?
-            let walk: String?
-        }
-        struct Pr: Codable {
-            let pr_short: String?
-        }
-        struct Code: Codable {
-            let areaname: String?
-            let prefname: String?
-            let areaname_s: String?
-            let category_name_l: [String]?
-        }
-    }
-    
-    struct ResultJson: Codable {
-        let rest:[RestJson]?
-    }
-    
-    func searchShop(keyword: String) {
-           guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-               return
+       let req = URLRequest(url: req_url)
+       let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+       let task = session.dataTask(with: req, completionHandler: {
+           (data, response, error) in
+           session.finishTasksAndInvalidate()
+           do {
+            let json = try? JSONDecoder().decode(ResultJson.self, from: data!)
+            
+
+            
+            if let restaurants = json?.rest {
+                self.restList.removeAll()
+                for rest in restaurants {
+                    self.restList.append(rest)
+                }
+                if let restdbg = self.restList.first {
+                    print("------------")
+                    print("okashiList[0] = \(restdbg)")
+                }
+                
+
+                
+//                    var config = Realm.Configuration()
+//                    config.deleteRealmIfMigrationNeeded = true
+//                    let realm = try! Realm(configuration: config)
+//                    print(Realm.Configuration.defaultConfiguration.fileURL!)
+//
+//                    try! realm.write {
+//                        let restFirst = RestGnavi(value: self.restList[0])
+//                        realm.add(restFirst)
+//                    }
+            
+                self.tableView.reloadData()
+                
+                if let restdbg = self.restList.first {
+                   print("------------")
+                   print("restList[0] = \(restdbg)")
+                }
+            }
+           } catch {
+               print("エラーが発生しました")
            }
-           guard let req_url = URL(string: "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=6bf4be830455182560cb7ad8ba817c16&freeword=\(keyword_encode)&hit_per_page=10") else {
-               return
-           }
-           print(req_url)
-           
-           let req = URLRequest(url: req_url)
-           let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-           let task = session.dataTask(with: req, completionHandler: {
-               (data, response, error) in
-               session.finishTasksAndInvalidate()
-               do {
-                   let decoder = JSONDecoder()
-                   let json = try decoder.decode(ResultJson.self, from: data!)
-//                   print(json)
-
-                   if let rests = json.rest {
-                       self.restList.removeAll()
-                       for rest in rests {
-                        if let id = rest.id, let name = rest.name, let category = rest.category, let url_mobile = rest.url_mobile, let shop_image1 = rest.image_url?.shop_image1, let adress = rest.address, let tel = rest.tel, let line = rest.access?.line, let station = rest.access?.station, let walk = rest.access?.walk, let pr_short = rest.pr?.pr_short, let areaname = rest.code?.areaname, let prefname = rest.code?.prefname, let areaname_s = rest.code?.areaname_s, let category_name_l = rest.code?.category_name_l  {
-                               let shop = (id, name, category, url_mobile, shop_image1, adress, tel, line, station, walk, pr_short, areaname, prefname, areaname_s, category_name_l)
-                               self.restList.append(shop)
-                           }
-                       }
-
-                       self.tableView.reloadData()
-
-                       if let shopdbg = self.restList.first {
-                           print("------------")
-                           print("restList[0] = \(shopdbg)")
-                       }
-                   }
-               } catch {
-                   print("エラーが発生しました")
-               }
-           })
-           task.resume()
+       })
+       task.resume()
     }
+
+
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restList.count
+    }
+
+    func getImageByUrl(url: String) -> UIImage{
+        let url = URL(string: url)
+        do {
+            let data = try Data(contentsOf: url!)
+            return UIImage(data: data)!
+        } catch let err {
+            print("Error : \(err.localizedDescription)")
+        }
+        return UIImage()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestCell") as! RestCell
+        cell.barNameLabel.text = restList[indexPath.row].name
+        let image:UIImage = getImageByUrl(url: restList[indexPath.row].shop_image1)
+        cell.barImageView.image = image
+        cell.genreLabel.text = restList[indexPath.row].category
+        cell.budgetLabel.text = "平均予算 \(restList[indexPath.row].budget.description)円"
+        cell.accessLabel.text = "\(restList[indexPath.row].line)\(restList[indexPath.row].station) 徒歩\(restList[indexPath.row].walk)分"
+        cell.prLabel.text = restList[indexPath.row].pr_short
+        cell.addressLabel.text = restList[indexPath.row].address
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let link = URL(string: restList[indexPath.row].url_mobile)!
+        let safariViewController = SFSafariViewController(url: link)
+        safariViewController.delegate = self
+        present(safariViewController, animated: true, completion: nil)
+    }
+
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
+
