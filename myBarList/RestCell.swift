48dba20fad246277bcacda395ece0e06a3286217
fsy218 <999fsy218@gmail.com>
2020-02-14 23:24:36 +0900

    Create RestCell.swift

---
+++ myBarList/RestCell.swift
@@ -0,0 +1,21 @@
+//
+//  BarCell.swift
+//  myBarList
+//
+//  Created by 山岡峻輔 on 2020/02/06.
+//  Copyright © 2020 山岡峻輔. All rights reserved.
+//
+
+import UIKit
+
+class RestCell: UITableViewCell {
+    
+    @IBOutlet weak var barNameLabel: UILabel!
+    @IBOutlet weak var barImageView: UIImageView!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var budgetLabel: UILabel!
+    @IBOutlet weak var accessLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var prLabel: UILabel!
+    
+}
